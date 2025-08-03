14623d305f8324f27e52333306701f6b07cdb684
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-08-03 13:16:56 +0200

    Pioneering Slint GUI Initial situation. 20250803 1316CET SDvW

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-pub mod address_init;
-pub mod address_oper;
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., 
+// starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
 
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let address_infterface = address_init::Address::address_init();
-    println!("Address initialized: {:?}\n", address_infterface);
+slint::slint!{
+    import { TextEdit } from "std-widgets.slint";
 
-    let app = AppWindow::new()?;
+    // The exported component should inherit from Window to avoid the deprecation warning.
+    export component ExportedComponent inherits Window {
+        TextEdit {}
+    }
+}
 
-    // Populate the ComboBox with data
-    // app.set_combo_items(slint::ModelRc::from([
-    //     "De heer ".into(),
-    //     "Mevrouw ".into(),
-    // ]));
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
 
-    app.run()
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
 }
