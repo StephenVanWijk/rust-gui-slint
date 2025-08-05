639ce2346298b71ef1195d7380aae2cd67a7806f
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-08-05 16:50:02 +0200

    I am not going to finish this. Slint is not sufficient for desktop apps, the documenation is on component level okay, but I miss a clear view on an overal 'Slint way'. 20250805 1649CET SDvW

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,17 @@
 // starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{error::Error, rc::Rc};
+use std::{error::Error};
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui  = AppWindow::new()?;
 
-    let model_a: Rc<slint::VecModel<_>> = Rc::new(slint::VecModel::from(
-        vec![
+    ui.on_file_new(|| {
+        println!("New file action triggered");
+    });
 
-        ]
-    ));
-
-    {   
-        let model_b: Rc<slint::VecModel<_>> = model_a.clone(); 
-        ui.on_add_todo(move |s|{
-        model_b.push(TodoItem{ text: s, checked: false });
-        });
-    }
-
-    ui.set_todos(Into::into(model_a));
     ui.run()?;
-
     Ok(())
 }
